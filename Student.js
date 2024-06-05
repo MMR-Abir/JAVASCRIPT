94cb39dfefde0877fb611e3c0da41d90a5c4c533
Noman-2019 <nomandiu2019@gmail.com>
2024-06-05 13:15:52 +0600

    05 june

---
+++ Student.js
@@ -0,0 +1,9 @@
+const Student_Info = () => {
+    const name = "Rafi" ;
+    const address = "Savar" ;
+    return `Name : ${name} <br> Address : ${address}` ;
+}
+let Country = "BANGLADESH";
+let District = "DHAKA"
+export default Student_Info;
+export { Country, District};
