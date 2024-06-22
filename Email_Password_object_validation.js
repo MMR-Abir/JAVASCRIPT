3f420a833181aff87cf2d7422963f44227ee21bb
MMR-Aabir <mostafizurrahmanabir4444@gmail.com>
2024-06-22 20:11:09 +0600

    22june2024

--- Email_Password_object_validation.js
+++ Email_Password_object_validation.js
@@ -1,16 +1,20 @@
 class Naruto {
-    constructor (email){
+    constructor (email,password){
         this.gmail = email;
-      //  this.Zone = password;
+        this.Zone = password;
     }
 
     Confirmation(){
       let  pattern = /[a-z0-9]+@[a-z]+\.[a-z]/;
-    //   let pattern1 = /[0-9]/;
+       let pattern1 = /^[0-9]+$/;
       
 
         if(this.gmail.search(pattern) == -1){
                 alert("Enter Valid Email Address")
+        }
+
+        else if(this.Zone.search(pattern1) == -1){
+                alert("Use Only Number")
         }
 
         // else if(this.Zone.search(pattern1) < 6){
